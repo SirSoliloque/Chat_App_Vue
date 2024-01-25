9da3449fd7e48fe5b56d0e11237161e7cc7e4281
Julien Ait azzouzene <julien>
2024-01-25 16:00:05 +0100

    ✨ feat: display more infos for the message

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {computed} from 'vue';
+
+const props = defineProps({
+    message: {
+        type: Object,
+        required: true 
+    }
+});
+
+function formatDate(date){
+    let formattedDay = date.toLocaleDateString();
+    let formattedTime = date.toLocaleTimeString('default', {hour: '2-digit', minute: '2-digit'});
+    return `${formattedDay} à ${formattedTime}`;
+};
+
+const formattedDate = computed(() => {
+    const date = props.message.date;
+    return formatDate(date);
+});
+
+</script>
+
+<template>
+    <div class="flex">
+        <img class="bg-slate-600 h-10 w-10 rounded-full" :src="message.user.avatarUrl">
+        {{ message.user.username }}
+        <span class="text-xs text-opacity-40 text-gray-50">
+            
+            {{ formattedDate }}
+            <!--TODO format jj/mm/yyyy à hh:mm -->
+            <!-- Aujourd'hui si c'est aujourd'hui -->
+            <!-- Hier...-->
+        </span>
+    </div>
+
+    <div>
+        {{ message.text }}
+    </div>
+</template>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,14 +1,23 @@
 <script setup>
 import {ref} from 'vue'
+import ChatMessage from '@/components/ChatMessage.vue';
+
 const messageText = ref('je suis un message');
-const messageList = ref([])
+const messageList = ref([]);
 
 function addMessage(){
     if(! messageText.value){
         return;
     }
 
-    messageList.value.push(messageText.value);
+    messageList.value.push({
+        text: messageText.value,
+        date: new Date(),
+        user: {
+            username: 'PetitGens',
+            avatarUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/654c7554-888a-4fff-bc61-5785cc9b7fa9/de6e47n-dee882ce-0fe5-4ea2-9bc9-c06755e6bf70.png/v1/fill/w_894,h_894/shiny_eevee__dp_sprite__by_lazoofficial_de6e47n-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzYwMCIsInBhdGgiOiJcL2ZcLzY1NGM3NTU0LTg4OGEtNGZmZi1iYzYxLTU3ODVjYzliN2ZhOVwvZGU2ZTQ3bi1kZWU4ODJjZS0wZmU1LTRlYTItOWJjOS1jMDY3NTVlNmJmNzAucG5nIiwid2lkdGgiOiI8PTM2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.GaDuq0RJtLLlLp-cWnHgGCn8-j203qfxjAvVrvZkMZY'
+        }
+    });
     messageText.value = '';
 }
 
@@ -16,10 +25,10 @@ function addMessage(){
 
 <template>
     <div v-for="(message, index) in messageList" class="p-4" :key="index">
-        {{ message }}
+        <chat-message :message="message"></chat-message>
     </div>
     <div class="flex align-center">
         <textarea v-model="messageText" name="message" id="message" rows="1" class="text-black rounded-md"></textarea>
-        <button v-on:click="addMessage" class="p-2 bg-blue-600 rounded-md ml-3">Envoyer</button>
+        <button @click="addMessage" class="p-2 bg-blue-600 rounded-md ml-3">Envoyer</button>
     </div>
 </template>
