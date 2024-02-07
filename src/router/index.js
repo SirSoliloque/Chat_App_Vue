062c325bff76edca388fdaa4356273bf1bed6c6b
Julien Ait azzouzene <=>
2024-02-07 15:59:58 +0100

    🚧 wip: login (untested)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ChatView from '@/views/ChatView.vue';
 import RegisterView from '@/views/RegisterView.vue';
+import LoginView from '@/views/LoginView.vue';
+import { supabase } from '@/supabase';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +10,34 @@ const router = createRouter({
     {
       path: '/',
       name: 'chat',
-      component: ChatView
+      component: ChatView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     }
   ]
+});
+
+router.beforeEach(async (to, from, next) => {
+    const {data} = await supabase.auth.getSession();
+    const isLoggedIn = !!data.session;
+
+    if(to.matched.some(record => record.meta.requiresAuth) && !isLoggedIn) {
+        next({name: 'login'});
+        return;
+    }
+
+    next();
 });
 
 export default router;
---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import { ref } from 'vue';
+import { supabase } from '@/supabase';
+import { useRouter } from 'vue-router';
+
+const {push: routerPush} = useRouter();
+
+const email = ref('');
+const password = ref('');
+
+const onSubmit = async () => {
+    if(! email.value || ! password.value) {
+        return;
+    }
+    
+    const {error} = await supabase.auth.signInWithPassword({
+        email: email.value,
+        password: password.value
+    });
+
+    if(error) {
+        alert(error.message);
+    } else{
+        routerPush('/');
+    }
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="onSubmit" class="flex flex-col gap-2 p-4 mx-auto max-w-96">
+        <div class="flex flex-col">
+            <label for="email">Email</label>
+            <input type="email" id="email" v-model="email" class="p-2 bg-slate-500" required/>
+        </div>
+        <div class="flex flex-col">
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" v-model="password" class="p-2 bg-slate-500" required/>
+        </div>
+        <button type="submit" class="p-2 mt-4 bg-blue-500 rounded-md">Se connecter</button>
+    </form>
+</template>
